eccf545a319530c511b2438bfbd616edec88c6a1
José Manuel Cornelio <jose@cornelioweb.com>
2023-08-29 17:02:08 -0400

    GET / POST methods

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
-export function GET(){
-    return NextResponse.json('Getting tasks')
+export async function GET() {
+    const getTasks = await prisma.task.findMany()
+    return NextResponse.json(getTasks)
+}
+
+export async function POST(request) {
+    const data = await request.json()
+    const createTask = await prisma.task.create({data})
+    return NextResponse.json(createTask)
 }
