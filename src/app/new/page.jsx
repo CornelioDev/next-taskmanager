5ef759f914996969907215b1ff1adccc0175629c
José Manuel Cornelio <jose@cornelioweb.com>
2023-08-31 16:54:08 -0400

    Create task form UI

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,12 +1,28 @@
+'use client'
 import React from 'react'
+
+export const metadata = {
+  title: 'Create New Task'
+}
+
+const submitHandler = (e) => {
+  e.preventDefault()
+  console.log(e.target.title.value, e.target.description.value)
+}
 
 const NewTask = () => {
   return (
-    <form>
-      <input type="text" id='title'/>
-      <textarea id="description" rows="10"></textarea>
-      <button>Create Task</button>
-    </form>
+    <div className='h-screen flex flex-col mt-28 items-center gap-4'>
+      <h1 className='text-4xl font-bold text-slate-100'>{metadata.title}</h1>
+      <form className='bg-slate-600 p-10 rounded text-gray-600 text-lg'
+        onSubmit={submitHandler}
+        action='/api/tasks'
+        method='POST'>
+        <input type="text" id='title' className='w-full mb-4 p-2 rounded' placeholder='Title' />
+        <textarea id="description" rows="3" className='w-full mb-3 rounded p-3' placeholder='Describe your task'></textarea>
+        <button type='submit' className='bg-sky-600 text-slate-100 p-2 rounded-md hover:bg-sky-800 transition-colors'>Create Task</button>
+      </form>
+    </div>
   )
 }
 
