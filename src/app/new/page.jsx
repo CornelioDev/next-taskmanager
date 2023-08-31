81498c21ec3054882df1847b11c92753e9ef0cf2
José Manuel Cornelio <cornelio@proton.me>
2023-08-31 19:20:09 -0400

    Create task function

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -5,19 +5,26 @@ export const metadata = {
   title: 'Create New Task'
 }
 
-const submitHandler = (e) => {
+const createTask = async (e) => {
   e.preventDefault()
-  console.log(e.target.title.value, e.target.description.value)
+
+  const title = e.target.title.value
+  const description = e.target.description.value
+  
+  await fetch('api/tasks', {
+    method: 'POST',
+    body: JSON.stringify({title, description}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 }
 
 const NewTask = () => {
   return (
     <div className='h-screen flex flex-col mt-28 items-center gap-4'>
       <h1 className='text-4xl font-bold text-slate-100'>{metadata.title}</h1>
-      <form className='bg-slate-600 p-10 rounded text-gray-600 text-lg'
-        onSubmit={submitHandler}
-        action='/api/tasks'
-        method='POST'>
+      <form className='bg-slate-600 p-10 rounded text-gray-600 text-lg' onSubmit={createTask}>
         <input type="text" id='title' className='w-full mb-4 p-2 rounded' placeholder='Title' />
         <textarea id="description" rows="3" className='w-full mb-3 rounded p-3' placeholder='Describe your task'></textarea>
         <button type='submit' className='bg-sky-600 text-slate-100 p-2 rounded-md hover:bg-sky-800 transition-colors'>Create Task</button>
