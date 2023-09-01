b44765d4b31981a37f2229f2932337edee1dac96
José Manuel Cornelio <cornelio@proton.me>
2023-08-31 21:57:44 -0400

    Tasks list

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const HomePage = () => {
+async function loadTasks() {
+  const response = await fetch('http://localhost:3000/api/tasks')
+  const data = await response.json()
+  return data
+}
+const HomePage = async () => {
+
+  const tasks = await loadTasks()
+
   return (
-    <h1 className='text-4xl'>Hello World!</h1>
+    <div className='container mx-auto grid grid-cols-3 mt-5 gap-3 text-slate-100'>
+      {tasks.map((task) => (
+        <div key={task.id} className='bg-slate-600 hover:bg-slate-700 p-3 flex flex-col hover:cursor-pointer'>
+          <h1 className='font-bold text-xl'>{task.title}</h1>
+          <p>{task.description ? task.description : <span className='font-light text-gray-400'>No description</span>}</p>
+          <p className='text-sm font-light text-gray-400 place-self-end'>{new Date(task.createdAt).toLocaleDateString()}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
