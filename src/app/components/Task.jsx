e3ebe282af30e85b50b8c3a5730c270ffd5f749d
José Manuel Cornelio <jose@cornelioweb.com>
2023-09-01 16:09:57 -0400

    Task component

---
+++ src/app/components/Task.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Task = ({ task }) => {
+    return (
+        <Link href={`/tasks/edit/${task.id}`}>
+            <div className='bg-slate-600 hover:bg-slate-700 p-3 flex flex-col hover:cursor-pointer place-content-between'>
+                <div>
+                    <h1 className='font-bold text-xl'>{task.title}</h1>
+                    <p>{task.description ? task.description : <span className='font-light text-gray-400'>No description</span>}</p>
+                </div>
+                <p className='text-sm font-light text-gray-400 place-self-end'>{new Date(task.createdAt).toLocaleDateString()}</p>
+            </div>
+        </Link>
+    )
+}
+
+export default Task
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Task from './components/Task'
 
 async function loadTasks() {
   const response = await fetch('http://localhost:3000/api/tasks')
@@ -11,13 +12,7 @@ const HomePage = async () => {
   return (
     <div className='container mx-auto grid grid-cols-3 mt-5 gap-3 text-slate-100'>
       {tasks.map((task) => (
-        <div key={task.id} className='bg-slate-600 hover:bg-slate-700 p-3 flex flex-col hover:cursor-pointer place-content-between'>
-          <div>
-            <h1 className='font-bold text-xl'>{task.title}</h1>
-            <p>{task.description ? task.description : <span className='font-light text-gray-400'>No description</span>}</p>
-          </div>
-          <p className='text-sm font-light text-gray-400 place-self-end'>{new Date(task.createdAt).toLocaleDateString()}</p>
-        </div>
+        <Task key={task.id} task={task} />
       ))}
     </div>
   )
